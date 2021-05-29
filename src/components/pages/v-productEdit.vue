86eb6463ad1c6b47deae837ea7dd709a9ee34fdc
Igor Levin <lytghzys@gmail.com>
2021-05-29 16:56:43 +0300

    Залил на хостинг

---
+++ src/components/pages/v-productEdit.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="v-productEdit">
+     <v-header></v-header>
+     <div class="productEditContainer">
+         
+     </div>
+     <v-footer></v-footer>
+</div>
+   
+
+</template>
+
+<script>
+
+import vHeader from '../v-header'
+import vFooter from '../v-footer'
+export default({
+    
+data(){},
+components:{
+vHeader,
+vFooter
+}
+
+
+})
+</script>
+
+<style scoped>
+
+</style>
